c7c02eec1851f3ac9072eb8055db566393819b72
wells <qishan.wu.1@lexisnexis.com>
2016-08-03 09:09:42 +0800

    refs: test

--- test/index.php
+++ test/index.php
@@ -1,5 +1,19 @@
-<?php
+<script src="jquery.js"></script>
+<script src="template.js"></script>
+<script>
+$(function (){
+	
+	var tpl = $('#tpl').html();
+	var a = template(tpl, {name: 'abc', title: '123'});
+	$("#test").html(a);
+});
+</script>
+<script id="tpl" type="text/html">
+<ul>
+    <li>{{name}}</li>
+	<li>{{title}}</li>
+</ul>
+</script>
 
-echo "sssssssssssss2eee";
-
-?>
+<div id="test">
+</div>
